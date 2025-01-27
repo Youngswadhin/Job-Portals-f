0b605da70fdc84bbccaa433ed8c00088b977a81c
Youngswadhin <swadhinsocialmedia@gmail.com>
2025-01-28 01:43:18 +0530

    adding auth

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,12 @@
-export const USER_API_END_POINT = `${import.meta.env.VITE_JOB_API_END_POINT}/api/v1/user`;
-export const JOB_API_END_POINT = `${import.meta.env.VITE_JOB_API_END_POINT}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${import.meta.env.VITE_JOB_API_END_POINT}/api/v1/application`;
-export const COMPANY_API_END_POINT = `${import.meta.env.VITE_JOB_API_END_POINT}/api/v1/company`;
+export const USER_API_END_POINT = `${
+  import.meta.env.VITE_JOB_API_END_POINT
+}/api/v1/user`;
+export const JOB_API_END_POINT = `${
+  import.meta.env.VITE_JOB_API_END_POINT
+}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${
+  import.meta.env.VITE_JOB_API_END_POINT
+}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${
+  import.meta.env.VITE_JOB_API_END_POINT
+}/api/v1/company`;
